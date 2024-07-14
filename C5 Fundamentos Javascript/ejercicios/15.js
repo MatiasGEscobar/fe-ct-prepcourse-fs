f972a1fe1e808d5806398a8ecf95bccc5fc340aa
MatiasGEscobar <matiie9135@gmail.com>
2024-07-14 15:23:14 -0300

    ejercicios clase 5 listos

--- C5 Fundamentos Javascript/ejercicios/15.js
+++ C5 Fundamentos Javascript/ejercicios/15.js
@@ -6,6 +6,7 @@ function esPar(num) {
   // 14 ---> true
   // 15 ---> false
   // Tu código:
+  return num%2===0
 }
 
 module.exports = esPar;
--- C5 Fundamentos Javascript/ejercicios/16.js
+++ C5 Fundamentos Javascript/ejercicios/16.js
@@ -6,6 +6,7 @@ function esImpar(num) {
   // 15 ---> true
   // 14 ---> false
   // Tu código:
+  return num%2!=0
 }
 
 module.exports = esImpar;
