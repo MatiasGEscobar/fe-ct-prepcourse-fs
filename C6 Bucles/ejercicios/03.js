9bbd3c2edcd66286989e023aa35a0f854291ccd0
MatiasGEscobar <matiie9135@gmail.com>
2024-07-15 22:31:41 -0300

    hasta el 11

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,8 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x!=y) return Math.max (x,y);
+  else if (x===y) return x||y;
 }
 
 module.exports = obtenerMayor;
